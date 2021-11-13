bd9f909184f27649afe4b257333dd29dde00c0e6
mshmwr <mshmwr20@gmail.com>
2021-11-14 02:45:52 +0800

    add: utils

---
+++ yo-taiwan/src/utils/addressUtils.js
@@ -0,0 +1,25 @@
+const cityTitleList = ["市", "縣"];
+const distTitleList = ["市", "鎮", "鄉", "區"];
+
+const splitAddressToCityAndDistrict = (address) => {
+  const getTitleIndex = (address, titleList) => {
+    for (let i = 0; i < titleList.length; i++) {
+      const index = address.indexOf(titleList[i]);
+      if (index > -1) {
+        return index;
+      }
+    }
+    return -1;
+  };
+
+  const cityIndex = getTitleIndex(address, cityTitleList);
+  const cityStr = address.substring(0, cityIndex + 1);
+
+  const distAddress = address.substring(cityIndex + 1);
+  const distIndex = getTitleIndex(distAddress, distTitleList);
+  const distStr = distAddress.substring(0, distIndex + 1).replace(/\d+/, "");
+
+  return `${cityStr}, ${distStr}`;
+};
+
+export { splitAddressToCityAndDistrict };
---
+++ yo-taiwan/src/utils/iconUtilis.js
@@ -0,0 +1,29 @@
+import { ReactComponent as IconWeatherSunny } from "../images/icon/sunny.svg";
+import { ReactComponent as IconWeatherRainy } from "../images/icon/rainy.svg";
+import { ReactComponent as IconWeatherCloudy } from "../images/icon/cloudy.svg";
+import { ReactComponent as IconBus } from "../images/icon/bus.svg";
+import { ReactComponent as IconLocation } from "../images/icon/location.svg";
+
+function getWeatherIcon(weather = null) {
+  switch (weather) {
+    case "sunny":
+      return <IconWeatherSunny alt={`weather-${weather}`} />;
+    case "rainy":
+      return <IconWeatherRainy alt={`weather-${weather}`} />;
+    case "cloudy":
+      return <IconWeatherCloudy alt={`weather-${weather}`} />;
+
+    default:
+      return <></>;
+  }
+}
+
+function getBusIcon() {
+  return <IconBus alt="bus" />;
+}
+
+function getLocationIcon() {
+  return <IconLocation alt="location" />;
+}
+
+export { getWeatherIcon, getBusIcon, getLocationIcon };
