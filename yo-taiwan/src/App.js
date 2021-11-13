6fada852d856aa18e1063c42f8d4980b5049d72d
mshmwr <mshmwr20@gmail.com>
2021-11-14 02:40:50 +0800

    install: react-router-dom, add homepage and routes

--- yo-taiwan/src/App.js
+++ yo-taiwan/src/App.js
@@ -1,7 +1,13 @@
 import "./css/reset.css";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
 
 function App() {
-  return <div>Home page</div>;
+  return (
+    <Routes>
+      <Route path="/" exact element={<HomePage />} />
+    </Routes>
+  );
 }
 
 export default App;
