4fc13849dfb1e1b356e78b7050cfb14c9cd77106
mshmwr <mshmwr20@gmail.com>
2021-11-27 23:52:03 +0800

    modify: remove YoBikePage

--- src/App.js
+++ src/App.js
@@ -4,21 +4,18 @@ import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import TripInfoPage from "./pages/TripInfoPage";
 import SearchingResult from "./pages/SearchingResult";
-import YoBikePage from "./pages/YoBikePage";
 
 function App() {
   return (
     <div>
       <Routes>
-        {/* <Route path="/" exact element={<HomePage />} /> */}
-        <Route path="/" exact element={<YoBikePage />} />
+        <Route path="/" exact element={<HomePage />} />
         <Route path="/searchingResult" element={<SearchingResult />} />
         <Route
           path="/searchingResult/:keywords"
           element={<SearchingResult />}
         />
         <Route path="/tripInfoPage" element={<TripInfoPage />} />
-        <Route exact path="/yoBike" element={<YoBikePage />} />
       </Routes>
     </div>
   );
