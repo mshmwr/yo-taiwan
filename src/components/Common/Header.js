0a97024ba5cf003387aaa2e28cbc8e8619a628e5
elsa <elsa2700@gmail.com>
2021-11-28 17:21:07 +0800

    導覽列下拉框

--- src/components/Common/Header.js
+++ src/components/Common/Header.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import waveBlue from "../../images/icon/wave_blue.png";
 import waveYellow from "../../images/icon/wave_yellow.png";
 import wavePink from "../../images/icon/wave_pink.png";
 import { getyoTaiwanLogoIcon } from "../../utils/iconUtilis";
 import HeaderSearch from "./HeaderSearch";
+import SelectBar from "./selectBar";
+import { SelectBarTraffic } from "./selectBar";
 
 const Header = ({ showSearch }) => {
+  const [showSelect, setshowSelect] = useState("hide");
+  const [showSelectTraffic, setshowSelectTraffic] = useState("hide");
+  function MouseOverSelect() {
+    setshowSelect("show");
+  }
+  function ClickSelect() {
+    setshowSelect("hide");
+  }
+  function MouseUpSelect() {
+    setshowSelect("show");
+  }
+  function MouseOverSelectTraffic() {
+    setshowSelectTraffic("show");
+  }
+  function ClickSelectTraffic() {
+    setshowSelectTraffic("hide");
+  }
+  function MouseUpSelectTraffic() {
+    setshowSelectTraffic("show");
+  }
   return (
     <div className="header">
-      <div className="header_container">
+      <div className="header_container" onClick={ClickSelectTraffic}>
         <Link to="/">{getyoTaiwanLogoIcon()}</Link>
         <div className={showSearch === "show" ? "show" : "hide"}>
           <HeaderSearch />
@@ -19,7 +41,13 @@ const Header = ({ showSearch }) => {
             觀光景點
             <img className="wave_blue1" src={waveBlue} alt="wave_blue" />
           </Link>
-          <Link to="/" className="menu_fair">
+          <Link
+            to="/"
+            className="menu_fair"
+            onMouseOver={MouseOverSelect}
+            onMouseUp={MouseUpSelect}
+            onClick={ClickSelect}
+          >
             主題旅遊
             <img className="wave_yellow" src={waveYellow} alt="wave_Yellow" />
           </Link>
@@ -27,11 +55,23 @@ const Header = ({ showSearch }) => {
             特色美食
             <img className="wave_pink" src={wavePink} alt="wave_Pink" />
           </Link>
-          <Link to="/yoBike" className="menu_traffic">
+          <Link
+            to="/"
+            className="menu_traffic"
+            onMouseOver={MouseOverSelectTraffic}
+            onMouseUp={MouseUpSelectTraffic}
+            onClick={ClickSelectTraffic}
+          >
             交通資訊
             <img className="wave_blue2" src={waveBlue} alt="wave_blue" />
           </Link>
           <button className="btn_callforaction">今天想去哪？</button>
+          <div className={showSelect === "show" ? "show" : "hide"}>
+            <SelectBar />
+          </div>
+          <div className={showSelectTraffic === "show" ? "show" : "hide"}>
+            <SelectBarTraffic />
+          </div>
         </div>
       </div>
     </div>
--- src/utils/iconUtilis.js
+++ src/utils/iconUtilis.js
@@ -6,6 +6,13 @@ import { ReactComponent as IconLocation } from "../images/icon/location.svg";
 import { ReactComponent as IconMyLocation } from "../images/icon/my_location.svg";
 import { ReactComponent as IconYoTaiwanLogo } from "../images/icon/YoTaiwan_logo.svg";
 import { ReactComponent as IconYoBikeLogo } from "../images/icon/YoBike_logo.svg";
+import { ReactComponent as IconCamping } from "../images/icon/camping.svg";
+import { ReactComponent as IconTemple } from "../images/icon/temple.svg";
+import { ReactComponent as IconOnsen } from "../images/icon/onsen.svg";
+import { ReactComponent as IconDiving } from "../images/icon/diving.svg";
+import { ReactComponent as IconBook } from "../images/icon/book.svg";
+import { ReactComponent as IconBicycle } from "../images/icon/dropdown_bicycle.svg";
+import { ReactComponent as IconBusSelect } from "../images/icon/dropdown_bus.svg";
 
 function getWeatherIcon(weather = null) {
   switch (weather) {
@@ -23,6 +30,30 @@ function getWeatherIcon(weather = null) {
 
 function getBusIcon() {
   return <IconBus alt="bus" />;
+}
+
+function getIconBicycle() {
+  return <IconBicycle alt="Bicycle" />;
+}
+
+function getIconBusSelect() {
+  return <IconBusSelect alt="bus" />;
+}
+
+function getCampingIcon() {
+  return <IconCamping alt="Camping" />;
+}
+function getIconTemple() {
+  return <IconTemple alt="Temple" />;
+}
+function getIconOnsen() {
+  return <IconOnsen alt="Onsen" />;
+}
+function getIconDiving() {
+  return <IconDiving alt="Diving" />;
+}
+function getIconBook() {
+  return <IconBook alt="Book" />;
 }
 
 function getLocationIcon() {
@@ -48,4 +79,11 @@ export {
   getMyLocationIcon,
   getyoTaiwanLogoIcon,
   getYoBikeLogoIcon,
+  getCampingIcon,
+  getIconTemple,
+  getIconOnsen,
+  getIconDiving,
+  getIconBook,
+  getIconBicycle,
+  getIconBusSelect,
 };
