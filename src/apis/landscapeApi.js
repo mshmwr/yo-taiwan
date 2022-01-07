5e98c45c9e8f3583ebb296f01f990ebf01c0d805
mshmwr <mshmwr20@gmail.com>
2022-01-07 20:05:01 +0800

    add: homepage landscape slider

---
+++ src/apis/landscapeApi.js
@@ -0,0 +1,48 @@
+import axios from "axios";
+import { getAuthorizationHeader } from "./axios";
+
+const getLandScapeAll = async () => {
+  let res = null;
+  try {
+    await axios
+      .get(
+        `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24format=JSON`,
+        {
+          headers: getAuthorizationHeader(),
+        }
+      )
+      .then(function (response) {
+        res = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    alert("GET Error!!" + error);
+  }
+  return res;
+};
+
+const getLandScape = async (startIndex, top = 5) => {
+  let res = null;
+  try {
+    await axios
+      .get(
+        `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?%24top=${top}&%24skip=${startIndex}&%24format=JSON`,
+        {
+          headers: getAuthorizationHeader(),
+        }
+      )
+      .then(function (response) {
+        res = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    alert("GET Error!!" + error);
+  }
+  return res;
+};
+
+export { getLandScape, getLandScapeAll };
--- src/components/HomePage/LandScape.js
+++ src/components/HomePage/LandScape.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { doSearch } from "../../apis/searchApi";
+import { getLandScape, getLandScapeAll } from "../../apis/landscapeApi";
 import {
   getWeatherIcon,
   getBusIcon,
@@ -9,18 +9,45 @@ import {
 import { splitAddressToCityAndDistrict } from "../../utils/addressUtils";
 import btn_next from "../../images/icon/btn_next.png";
 
+const landscapeQuantity = 5;
+
 const LandScape = () => {
-  const [LandScapeItem, setLandScapeItem] = useState();
+  const [totalPage, settotalPage] = useState(0);
+
+  const [currentPage, setcurrentPage] = useState(0);
+  const [LandScapeItem, setLandScapeItem] = useState([]);
+
+  const handleClickNext = () => {
+    setcurrentPage((prev) => prev + 1);
+  };
+  const handleClickPrev = () => {
+    setcurrentPage((prev) => prev - 1);
+  };
+
   useEffect(() => {
     async function fetchData() {
-      setLandScapeItem(await doSearch(5));
+      setLandScapeItem(await getLandScape(currentPage * landscapeQuantity));
+    }
+    fetchData();
+  }, [currentPage]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const landscapes = await getLandScapeAll(currentPage * landscapeQuantity);
+      console.log("len, ", landscapes.length);
+      settotalPage(Math.ceil(landscapes.length / landscapeQuantity));
     }
     fetchData();
   }, []);
+
   return (
     <div className="landscape_section">
       <span className="section_title">想去哪玩？</span>
-
+      {!!currentPage && (
+        <div className="btn_prev" onClick={handleClickPrev}>
+          <img src={btn_next} alt="btn_prev" />
+        </div>
+      )}
       {LandScapeItem &&
         LandScapeItem.map((item) => {
           return (
@@ -36,7 +63,7 @@ const LandScape = () => {
                     <img alt={item.Name} src={item.Picture.PictureUrl1} />
                   </div>
                   <div className="content_block">
-                    {item.Name}
+                    {item.ScenicSpotName}
                     {item?.Bus && (
                       <div className="tag_bus">
                         {getBusIcon()}
@@ -56,9 +83,11 @@ const LandScape = () => {
             </Link>
           );
         })}
-      <div className="btn_next">
-        <img src={btn_next} alt="btn_next" />
-      </div>
+      {currentPage < totalPage && (
+        <div className="btn_next" onClick={handleClickNext}>
+          <img src={btn_next} alt="btn_next" />
+        </div>
+      )}
     </div>
   );
 };
