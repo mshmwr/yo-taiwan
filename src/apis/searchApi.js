80b23099336de8e253327e0d19eb26427e25646e
Elsa Lan <chiaowebsite@gmail.com>
2022-03-12 18:39:08 +0800

    完成主題旅遊頁面

--- src/apis/searchApi.js
+++ src/apis/searchApi.js
@@ -22,6 +22,28 @@ const doSearch = async (top = 30) => {
   return res;
 };
 
+const doSearchTopic = async (topicName = "") => {
+  let res = null;
+  try {
+    await axios
+      .get(
+        `https://ptx.transportdata.tw/MOTC/v2/Tourism/ScenicSpot?$filter=contains(DescriptionDetail,%20%27${topicName}%27)&$top=10&$format=JSON`,
+        {
+          headers: getAuthorizationHeader(),
+        }
+      )
+      .then(function (response) {
+        res = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (error) {
+    alert("GET Error!!" + error);
+  }
+  return res;
+};
+
 const doSearchName = async (name = "", address = "", top = 30) => {
   let res = null;
   try {
@@ -44,4 +66,4 @@ const doSearchName = async (name = "", address = "", top = 30) => {
   return res;
 };
 
-export { doSearch, doSearchName };
+export { doSearch, doSearchName, doSearchTopic };
--- src/components/TopicTab/index.js
+++ src/components/TopicTab/index.js
@@ -5,6 +5,7 @@ import onsenSection from "../../asset/images/onsenSection.png";
 import divingSection from "../../asset/images/divingSection.png";
 import bookSection from "../../asset/images/bookSection.png";
 import "./style.scss";
+import SearchingResult from "../../pages/TravelFeaturedPage/searchingResult";
 import {
   getCampingIcon,
   getIconTemple,
@@ -20,30 +21,35 @@ const TopicTab = () => {
       icon: getCampingIcon(),
       sectionImg: campingSection,
       word: "# 與三五好友一起呼吸大自然",
+      keywords: ["露營"]
     },
     {
       name: "古蹟巡禮",
       icon: getIconTemple(),
       sectionImg: templeSection,
       word: "# 一同探訪漫長歷史演進所留下的美麗文化資產",
+      keywords: ["古蹟"],
     },
     {
       name: "溫泉療癒",
       icon: getIconOnsen(),
       sectionImg: onsenSection,
       word: "# 凜凜冬日，就要來趟溫泉輕旅行！",
+      keywords: ["溫泉"],
     },
     {
       name: "海洋探索",
       icon: getIconDiving(),
       sectionImg: divingSection,
       word: "# 與美麗湛藍的海洋來場浪漫約會",
+      keywords: ["海岸"],
     },
     {
       name: "知性之旅",
       icon: getIconBook(),
       sectionImg: bookSection,
       word: "# 讓知識的力量陶冶你的心靈",
+      keywords: ["知識"],
     },
   ];
   const [selectedTab, setselectedTab] = useState(0);
@@ -90,6 +96,7 @@ const TopicTab = () => {
         <div class="section_topic">{tabs[selectedTab].name}</div>
         <div class="section_subtitle">{tabs[selectedTab].word}</div>
       </div>
+      <SearchingResult searchTopic={tabs[selectedTab]} />
     </div>
   );
 };
