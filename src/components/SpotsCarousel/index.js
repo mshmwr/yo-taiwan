906d114c71e6f088ab82fd72746fcc39096dee66
mshmwr <mshmwr20@gmail.com>
2022-04-05 15:57:02 +0800

    add: fetched item number depends on screen width

--- src/components/SpotsCarousel/index.js
+++ src/components/SpotsCarousel/index.js
@@ -1,12 +1,24 @@
 import React, { useState, useEffect } from "react";
 import btn_next from "../../asset/icon/btn_next.png";
 import Spots from "./Spots";
-const SPOT_QUANTITY = 5;
+const SPOT_QUANTITY_DESKTOP = 5;
+const SPOT_QUANTITY_MOBILE = 4;
+function handleWidthChange(width) {
+  return width > 1200;
+}
+
+let prevWidth = 0;
 
 const SpotsCarousel = ({ title = "", page = "", fetchSpot, fetchSpotAll }) => {
   const [totalPage, setTotalPage] = useState(0);
   const [currentPage, setCurrentPage] = useState(0);
   const [spots, setSpots] = useState([]);
+  const [isDesktop, setIsDesktop] = useState(true);
+
+  const SPOT_QUANTITY = isDesktop
+    ? SPOT_QUANTITY_DESKTOP
+    : SPOT_QUANTITY_MOBILE;
+
   const handleClickNext = () => {
     setCurrentPage((prev) => prev + 1);
   };
@@ -16,10 +28,10 @@ const SpotsCarousel = ({ title = "", page = "", fetchSpot, fetchSpotAll }) => {
 
   useEffect(() => {
     async function fetchData() {
-      setSpots(await fetchSpot(currentPage * SPOT_QUANTITY));
+      setSpots(await fetchSpot(currentPage * SPOT_QUANTITY, SPOT_QUANTITY));
     }
     if (fetchSpot) fetchData();
-  }, [currentPage, fetchSpot]);
+  }, [currentPage, fetchSpot, SPOT_QUANTITY]);
 
   useEffect(() => {
     async function fetchData() {
@@ -28,6 +40,20 @@ const SpotsCarousel = ({ title = "", page = "", fetchSpot, fetchSpotAll }) => {
     }
     if (fetchSpotAll) fetchData();
   }, []);
+
+  useEffect(() => {
+    const observer = new ResizeObserver((entries) => {
+      for (const entry of entries) {
+        const width = entry.borderBoxSize?.[0].inlineSize;
+        if (typeof width === "number" && width !== prevWidth) {
+          prevWidth = width;
+          setIsDesktop(handleWidthChange(width));
+        }
+      }
+    });
+
+    observer.observe(document.body, { box: "border-box" });
+  });
 
   return (
     <div className="landscape_section">
